3da4b1205f70cbd1a872edc913868d4698bee713
samteck <samadoye28@gmail.com>
2025-03-16 17:08:21 +0100

    committing all messages

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,57 @@
-import Image from "next/image";
+'use client';
+
+import {useEffect, useState} from 'react';
 
 export default function Home() {
+  
+  const [name, setName] = useState('');
+  const [deleteNameEl, setDeleteNameEl] = useState('')
+  const [people, setPeople] = useState(['Pls wait']);
+
+  const fetchData = () => {
+    fetch("http://localhost:8080/api/home").then(
+      response => response.json()
+    ).then(data => {
+      setPeople(data.people)
+    })  
+  }
+  const postName =()=> {
+    fetch("http://localhost:8080/api/home", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({name})
+    }).then(response => response.json()).then().catch(err => console.log('An Error Occurred:', err))
+  }
+  const deleteName =()=> {
+    fetch("http://localhost:8080/api/home/" + deleteNameEl , {
+      method: 'DELETE',
+    }).then(response => response).then()
+      .catch(err => console.log('An error occurred:', err))
+  }
+  
+  useEffect(()=>{
+    setInterval(()=> fetchData(), 1000)
+  }, [])
+
   return (
     <section>
-      learn next
+      {people.map((person, index) => <p key={index}>{person}</p>)} 
+      <form>
+        <input type="text" placeholder='Add Name' onChange={(e)=>setName(e.target.value)} value={name}/>
+        <button onClick={(e)=>{
+          e.preventDefault();
+          postName();
+          setName('');
+        }} >Add Name</button>
+        <button onClick={(e)=>{
+          deleteName();
+          console.log('User Deleted ' + deleteNameEl );
+          e.preventDefault();
+          setDeleteNameEl('');
+        }} >Delete Name</button>
+      </form>
     </section>
   );
 };
--- server/index.js
+++ server/index.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const { name } = require('ejs');
 const PORT = 8080;
 
+app.use(cors());
+app.use(express.json());
+
+let products = {
+    people: ['Proff', 'Awaal', 'Samad']
+}
+
 app.get('/api/home', (req, res) => {
-    res.json({message: 'Hello world'})
+    res.json(products)
 });
+app.post('/api/home', (req, res) => {
+    const content = req.body.name;
+    products.people.push(content);
+    res.json(products);
+})
+app.delete('/api/home/:deleteName', (req, res)=>{
+    const deleteName = req.params.deleteName;
+    products.people = products.people.filter(person => {
+        return person !== deleteName
+    })
+    res.json(products);
+})
 
 app.listen(PORT, ()=>{
-    
+    console.log('Server started on port ' + PORT);
 })
